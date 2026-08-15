0e4ec0195d5e15c885c01d947a8dd0ef2920f511
Ameen2425 <abdulameen200@gmail.com>
2026-08-16 01:13:10 +0530

    update

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { AppRouter } from "../AppRouter/AppRouter";
 import "./MainLayout.css";
 import Header from "../components/Header/Header";
@@ -7,6 +8,15 @@ import Footer from "../components/Footer/Footer";
 const MainLayout = () => {
 
   const location = useLocation();
+
+  // Automatically scroll to the top of the page on route change
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant"
+    });
+  }, [location.pathname]);
 
   const pathLoc = [
     "/login",
